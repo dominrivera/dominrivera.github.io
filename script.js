d59aaee4aaf5c18d7ee492d788c8d8d83e0e2667
Domingo Rivera <domingo.rivera.barros@cern.ch>
2023-07-28 12:19:56 +0200

    Add intro message and dummy experience

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const translations = {
         "en": `
             <img src="fotocv1.jpeg" alt="Your picture" class="card-img">
             <h1 class="card-name">Hi! I am Domingo.</h1>
-            <p class="card-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+            <h2 class="card-role">Computing Engineer</h2>
+            <p class="card-desc">I'm Domingo, a passionate technologist from Spain, currently living in the city of Geneva, Switzerland. My professional focus is on infrastructure, systems, and DevOps, where I use my expertise to develop efficient and robust solutions. I'm driven by the endless possibilities of technology and its potential to drive meaningful change. Nice to meet you!</p>
             <div class="card-links">
                 <a href="domingorb_cv.pdf" title="Resume" target="_blank">
                     <i class="fas fa-file-alt link-icon"></i>
@@ -26,7 +27,8 @@ const translations = {
         "es": `
             <img src="fotocv1.jpeg" alt="Your picture" class="card-img">
             <h1 class="card-name">Hola! Soy Domingo.</h1>
-            <p class="card-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+            <h2 class="card-role">Ingeniero Informatico</h2>
+            <p class="card-desc">Soy Domingo, un español apasionado por la tecnología, actualmente viviendo en la ciudad de Ginebra, Suiza. Mi enfoque profesional está en la infraestructura, los sistemas y DevOps, donde utilizo mi experiencia para desarrollar soluciones eficientes y robustas. Me motiva las infinitas posibilidades de la tecnología y su potencial para impulsar cambios significativos. ¡Encantado de conocerte!</p>
             <div class="card-links">
                 <a href="domingorb_cv.pdf" title="Curriculum" target="_blank">
                     <i class="fas fa-file-alt link-icon"></i>
@@ -49,7 +51,35 @@ const translations = {
     "experience": {
         "en": `
             <h2>Experience</h2>
-            <p class="card-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+            <div class="experience-entry">
+                <h3>Senior DevOps Engineer</h3>
+                <h4>XYZ Company, Geneva - Jan 2021 to Present</h4>
+                <ul class="achievements-list">
+                    <li>Led a team to implement CI/CD pipelines, reducing deployment times by 30%.</li>
+                    <li>Implemented automated testing strategies, improving software reliability by 25%.</li>
+                    <li>Reduced cloud infrastructure costs by 20% through efficient resource utilization.</li>
+                </ul>
+            </div>
+        
+            <div class="experience-entry">
+                <h3>Systems Administrator</h3>
+                <h4>ABC Corporation, Geneva - Jan 2019 to Dec 2020</h4>
+                <ul class="achievements-list">
+                    <li>Managed and maintained a large-scale Linux server environment, ensuring 99.99% uptime.</li>
+                    <li>Implemented a system monitoring solution which reduced system downtime by 15%.</li>
+                    <li>Automated routine tasks using shell scripting, saving approximately 10 hours of manual work per week.</li>
+                </ul>
+            </div>
+        
+            <div class="experience-entry">
+                <h3>Junior DevOps Engineer</h3>
+                <h4>DEF Software, Madrid - Jan 2017 to Dec 2018</h4>
+                <ul class="achievements-list">
+                    <li>Assisted in the deployment of applications in AWS environment.</li>
+                    <li>Developed scripts for build, deployment, maintenance, and related tasks.</li>
+                    <li>Helped in implementing DevOps principles and practices, reducing deployment failures by 20%.</li>
+                </ul>
+            </div>
         `,
         "es": `
             <h2>Experiencia</h2>
